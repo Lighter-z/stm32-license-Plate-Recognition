e088e8491fb2b4b6b4159312643f3c1b242d4980
Lighter-z <1106606588@qq.com>
2019-12-28 12:04:55 +0800

    Data_LCD_ColorChange函数中摄像头扫描用Data_LCD_ColorChange_Test替换

--- user/main.c
+++ user/main.c
@@ -1005,118 +1005,6 @@ void ChangePoint_Analysis_320()//蓝色区域中，320跳变点分析,获得TableChangePoint_
 	}
 }
 
-void Data_LCD_ColorChange()//摄像头扫描
-{
-	vu16 a=0,b=0;
-	vu8 i=0;
-	for(a=0;a<240;a++)//各行跳变点计数，数组清零
-	{
-		TableChangePoint_240[a]=0;
-	}
-	Min_blue=320;//初始化记录蓝色车牌区域的值
-	Max_blue=0;
-	
-	LCD_SetWindows(0,0,320,240);//设置显示窗口
-	GPIO_WriteBit(LCD_RS_PORT, LCD_RS_PIN,1);//标志：数据写入
-
-	while(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_0)==1);
-	GPIO_WriteBit(FIFO_WRST_PORT, FIFO_WRST_PIN, 0);
-	GPIO_WriteBit(FIFO_WRST_PORT, FIFO_WRST_PIN, 1);
-	GPIO_WriteBit(FIFO_WR_PORT, FIFO_WR_PIN, 1);
-	while(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_0)==0);
-	while(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_0)==1);
-	GPIO_WriteBit(FIFO_WR_PORT, FIFO_WR_PIN, 0);
-	
-	FIFO_Reset_Read_Addr(); 
- 
-	
-	for (a=0;a<240;a++) //扫描检测240  即宽度
-	{
-		for(b=0;b<320;b++)
-		{
-			GPIOC->BRR =1<<4;
-			AA=GPIOA->IDR;						
-			GPIOC->BSRR =1<<4;
-			
-			GPIOC->BRR =1<<4;
-			BB=GPIOA->IDR&0x00ff;								
-			GPIOC->BSRR =1<<4;	
-			
-			color=(AA<<8)|BB;
-
-			R=color>>11;
-			G=(color>>5)&0x3f;
-			B=color&0x1f;
-			
-			if((R>R_a) && (G>=G_a) && (B>=B_a))//二值化,高阈值：25.55.25，较合适阈值（21,47,21）
-			{
-				color=0xffff; //白色
-			}
-			else
-			{
-				color=0x0000; //黑色
-			}
-			
-			if(color!=color_save)//跳变点
-			{
-				TableChangePoint_240[a]++;		//该行跳变点计数+1
-			}
-			color_save=color;//保存像素值，供下一次判断和比较
-			
-			color=(AA<<8)|BB;//还原色彩
-					
-			LCD_DATA_PORT->ODR = color;
-			GPIOC->BRR =1<<11;
-			GPIOC->BSRR =1<<11;
-		}
-	}
-	
-	ChangePoint_Show_240();//240方向跳变点显示  
-	ChangePoint_Analysis_240();	//跳变点分析    获得车牌高度
-	//返回flag_MaxMinCompare，Min_ChangePoint_240，Max_ChangePoint_240
-	if(flag_MaxMinCompare==1)//跳变点筛选成功  即高度合理
-	{
-			BEEP_ON;
-			delayms(500);
-			BEEP_OFF;
-		//分析显示车牌左右边界
-		 ChangePoint_Analysis_Blue();//320蓝色区域分析,采用读取像素，得结果Min_blue,Max_blue  分析车牌左右边界
-		if(Min_blue>Max_blue) 
-			flag_MaxMinCompare=0;//进行合理性判断1
-		if((Min_blue>290)||(Max_blue>290)) 
-			flag_MaxMinCompare=0;//进行合理性判断2
-	}
-	if(flag_MaxMinCompare==1)//跳变点筛选成功  左右边界获取成功 并且合理
-	{
-		ChangePoint_Analysis_320();//蓝色区域中，320跳变点分析,获得：TableChangePoint_320[b]结果 左右边界内二值化
-		ChangePoint_Show_320();//320方向跳变点显示
-		i=ZhiFuFenGe(); 
-		
-		if(i==8)//字符分割,返回分割的字符个数，用于判断合法性
-		{
-			ZhiFuShiBie();//字符识别	
-		}
-		else
-		{
-			LCD_Fill(0x6666);//黑屏，显示Measure Faill
-			LCD_ShowChar(8*1,200,'M',0);
-			LCD_ShowChar(8*2,200,'e',0);
-			LCD_ShowChar(8*3,200,'a',0);
-			LCD_ShowChar(8*4,200,'s',0);
-			LCD_ShowChar(8*5,200,'u',0);
-			LCD_ShowChar(8*6,200,'r',0);
-			LCD_ShowChar(8*7,200,'e',0);
-			
-			LCD_ShowChar(8*9,200,'F',0);
-			LCD_ShowChar(8*10,200,'a',0);
-			LCD_ShowChar(8*11,200,'i',0);
-			LCD_ShowChar(8*12,200,'l',0);
-			LCD_ShowChar(8*13,200,'l',0);
-
-			delay_ms(800);
-		}
-	}
-}
 void Data_LCD_ColorChange_Test()//摄像头扫描测试
 {
 	vu16 a=0,b=0;
@@ -1183,8 +1071,59 @@ void Data_LCD_ColorChange_Test()//摄像头扫描测试
 		}
 	}
 	ChangePoint_Show_240();//240方向跳变点显示
-	ChangePoint_Analysis_240();	//跳变点分析
+	ChangePoint_Analysis_240();	//跳变点分析  获得车牌高度
 }
+
+void Data_LCD_ColorChange()//摄像头扫描
+{
+	vu8 i=0;
+
+	Data_LCD_ColorChange_Test();
+	//返回flag_MaxMinCompare，Min_ChangePoint_240，Max_ChangePoint_240
+	if(flag_MaxMinCompare==1)//跳变点筛选成功  即高度合理
+	{
+			BEEP_ON;
+			delayms(500);
+			BEEP_OFF;
+		//分析显示车牌左右边界
+		 ChangePoint_Analysis_Blue();//320蓝色区域分析,采用读取像素，得结果Min_blue,Max_blue  分析车牌左右边界
+		if(Min_blue>Max_blue) 
+			flag_MaxMinCompare=0;//进行合理性判断1
+		if((Min_blue>290)||(Max_blue>290)) 
+			flag_MaxMinCompare=0;//进行合理性判断2
+	}
+	if(flag_MaxMinCompare==1)//跳变点筛选成功  左右边界获取成功 并且合理
+	{
+		ChangePoint_Analysis_320();//蓝色区域中，320跳变点分析,获得：TableChangePoint_320[b]结果 左右边界内二值化
+		ChangePoint_Show_320();//320方向跳变点显示
+		i=ZhiFuFenGe(); 
+		
+		if(i==8)//字符分割,返回分割的字符个数，用于判断合法性
+		{
+			ZhiFuShiBie();//字符识别	
+		}
+		else
+		{
+			LCD_Fill(0x6666);//黑屏，显示Measure Faill
+			LCD_ShowChar(8*1,200,'M',0);
+			LCD_ShowChar(8*2,200,'e',0);
+			LCD_ShowChar(8*3,200,'a',0);
+			LCD_ShowChar(8*4,200,'s',0);
+			LCD_ShowChar(8*5,200,'u',0);
+			LCD_ShowChar(8*6,200,'r',0);
+			LCD_ShowChar(8*7,200,'e',0);
+			
+			LCD_ShowChar(8*9,200,'F',0);
+			LCD_ShowChar(8*10,200,'a',0);
+			LCD_ShowChar(8*11,200,'i',0);
+			LCD_ShowChar(8*12,200,'l',0);
+			LCD_ShowChar(8*13,200,'l',0);
+
+			delay_ms(800);
+		}
+	}
+}
+
  
  
 void Show_Card(vu8 i)//显示第几组车牌
@@ -1376,31 +1315,31 @@ int main(void)
 
 	while(1)
 	{
-						if(num<=1)
-						{
-							Data_LCD_ColorChange();//车牌测定
-						}
-						if(num>1)
-						{
-							Data_LCD_ColorChange_Test();//摄像头扫描测试
-							LCD_ShowNum(30,220,21-num,2);//
-							while(GPIO_ReadInputDataBit(KEY1_PORT,KEY1_PIN)==0)
-							{
-								LCD_Fill(0x00);//黑屏
-								Show_Title();//显示标题
-								Show_Card(0);//显示第几组车牌
-								Show_Card(1);
-								Show_Card(2);
-								Show_Card(3);
-								Show_Card(4);
-								delay_ms(5000);
-							}
-						}
-					if(num==20)
-					{
-						 num=0;
-					}
-		         num++;
+			if(num<=1)
+			{
+				Data_LCD_ColorChange();//车牌测定
+			}
+			if(num>1)
+			{
+				Data_LCD_ColorChange_Test();//摄像头扫描测试
+				LCD_ShowNum(30,220,21-num,2);//
+				while(GPIO_ReadInputDataBit(KEY1_PORT,KEY1_PIN)==0)
+				{
+					LCD_Fill(0x00);//黑屏
+					Show_Title();//显示标题
+					Show_Card(0);//显示第几组车牌
+					Show_Card(1);
+					Show_Card(2);
+					Show_Card(3);
+					Show_Card(4);
+					delay_ms(5000);
+				}
+			}
+		if(num==20)
+		{
+			 num=0;
+		}
+			 num++;
 	}
  
 }
